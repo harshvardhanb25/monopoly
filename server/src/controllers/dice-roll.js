c6882639cc9f65cfeac90321ce0c21d2e53e0747
harshvardhanb25 <harshvardhanb25@arizona.edu>
2023-05-16 00:34:59 -0700

    Add comments to dice-roll.js

--- server/src/controllers/dice-roll.js
+++ server/src/controllers/dice-roll.js
@@ -1,8 +1,15 @@
-//Roll two dice and return the result as a JSON object
-exports.roll = () => { 
+// Export a function called "roll"
+exports.roll = () => {
+    // This is a helper function that generates a random number between 1 and 6
     const rollDie = () => Math.floor(Math.random() * 6) + 1;
+
+    // Roll two dice by calling the rollDie function twice, and store the results
     const die1 = rollDie();
     const die2 = rollDie();
+
+    // Calculate the total of both dice rolls
     const total = die1 + die2;
+
+    // Return an object containing the values for each die and their total
     return { die1, die2, total };
-}
+};
