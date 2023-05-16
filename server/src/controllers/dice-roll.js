1abfc1abec7dd65697159005531ab5ee99643b99
harshvardhanb25 <harshvardhanb25@arizona.edu>
2023-05-16 00:32:38 -0700

    Refactor dice-roll controller to use a separate function for dice rolls
    This change adds a new function rollDie and uses it to roll two dice and return their sum as a JSON object. This improves the code readability and makes it easier to modify the roll mechanism in the future.

--- server/src/controllers/dice-roll.js
+++ server/src/controllers/dice-roll.js
@@ -1,5 +1,6 @@
 //Roll two dice and return the result as a JSON object
 exports.roll = () => { 
+    const rollDie = () => Math.floor(Math.random() * 6) + 1;
     const die1 = rollDie();
     const die2 = rollDie();
     const total = die1 + die2;
