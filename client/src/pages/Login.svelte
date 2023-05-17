c534ba96def03cf152c948ab9fb62df3dcf656e1
Parth Agarwal <agarwalparth777@gmail.com>
2023-05-17 02:12:17 -0700

    Login Page Markup

--- client/src/pages/Login.svelte
+++ client/src/pages/Login.svelte
@@ -1,1 +1,23 @@
-<h1>Hi From Login.Svelte</h1>
+<main class="container">
+	<article class="grid">
+		<div>
+			<hgroup style="text-align: center;">
+				<h1 style="margin-bottom: 1rem">Sign In</h1>
+				<h2>Sign In to ApnaPoly</h2>
+			</hgroup>
+			<form>
+				<input type="text" placeholder="Username" required />
+				<input type="password" placeholder="Password" required />
+				<button type="submit" class="contrast" onclick="event.preventDefault()">Sign In</button>
+			</form>
+		</div>
+	</article>
+</main>
+
+<style>
+    .container {
+        height: 100vh;
+        display: grid;
+        place-items: center;
+    }
+</style>
