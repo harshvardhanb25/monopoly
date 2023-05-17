b7797297181a953a9d0ad63d2a3ffe76427da731
Parth Agarwal <agarwalparth777@gmail.com>
2023-05-17 02:28:59 -0700

    Error Page Markup

--- client/src/pages/Error.svelte
+++ client/src/pages/Error.svelte
@@ -1,1 +1,27 @@
-<h1>Hi From Error.Svelte</h1>
+<main class="container">
+	<article class="grid">
+		<div>
+			<hgroup style="text-align: center;">
+				<h1 style="margin-bottom: 1rem">ERROR</h1>
+				<h2>Sorry, this page was not found!</h2>
+			</hgroup>
+			<form>
+				<a role="button" href="/#/login" type="submit" class="contrast">Return to Login</a>
+				<a role="button" href="/#/" type="submit">Return to Game</a>
+			</form>
+		</div>
+	</article>
+</main>
+
+<style>
+    .container {
+        height: 100vh;
+        display: grid;
+        place-items: center;
+    }
+
+    a {
+        display: block;
+        margin-bottom: 1rem;
+    }
+</style>
